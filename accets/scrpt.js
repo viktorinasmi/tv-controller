c16fdccda3f9c120b54afd48ce867172153b25c5
Viktoria Smirnova <bika0724@gmail.com>
2022-04-16 10:25:25 +0300

    Изменение использования с переменных на объекты Сброс цвета при нажатии на кнопку "ОК"

--- accets/scrpt.js
+++ accets/scrpt.js
@@ -1,112 +1,93 @@
 console.log("Файл загрузился");
-// Кнопка влючения/выключения
-var buttonPower = document.getElementById("btn-power")
 
-// Кнопка вверх
-var buttonArrowTop = document.getElementById("btn-arrow-top")
+var buttonElements = {
+    buttonPower: document.getElementById("btn-power"),  // Кнопка влючения/выключения
+    buttonArrowTop: document.getElementById("btn-arrow-top"), // Кнопка вверх
+    buttonArrowLeft: document.getElementById("btn-arrow-left"), // Кнопка влево
+    buttonCenter: document.getElementById("btn-center"), // Кнопка ОК
+    buttonArrowRight: document. getElementById("btn-arrow-right"), // Кнопка вправо
+    buttonArrowBottom: document.getElementById("btn-arrow-bottom"), // Кнопка вниз
+    buttonMainBack: document.getElementById("btn-main-back"), //Кнопка назад
+    buttonMainSearch: document.getElementById("btn-main-search"), //Кнопка поиск
+    buttonMainSetting: document.getElementById("btn-main-setting"), //Кнопка настройки
+    buttonMainVoice: document.getElementById("btn-main-voice"), //Кнопка микрофон
+    buttonMainTurnup: document.getElementById("btn-main-turnup"), //Кнопка прибавление звука
+    buttonMainTurndown: document.getElementById("btn-main-turndown"), //Кнопка убавления звука
+}
 
-// Кнопка влево
-var buttonArrowLeft = document.getElementById("btn-arrow-left")
 
-// Кнопка ОК
-var buttonCenter =document.getElementById("btn-center")
-
-// Кнопка вправо
-var buttonArrowRight = document. getElementById("btn-arrow-right")
-
-// Кнопка вниз
-var buttonArrowBotton = document.getElementById("btn-arrow-bottom")
-
-//Кнопка назад
-var buttonMainBack = document.getElementById("btn-main-back")
-
-//Кнопка поиск
-var buttonMainSearch = document.getElementById("btn-main-search")
-
-//Кнопка настройки
-var buttonMainSetting = document.getElementById("btn-main-setting")
-
-//Кнопка микрофон
-var buttonMainVoice = document.getElementById("btn-main-voice")
-
-//Кнопка прибавление звука
-var buttonMainTurnup = document.getElementById("btn-main-turnup")
-
-//Кнопка убавления звука
-var buttonMainTurndown = document.getElementById("btn-main-turndown")
-
-// Основной фон
-var view = document.getElementById("view")
-
-// Фон кружочка
-var viewCircle = document.getElementById("view-circle")
+var mainElements = {
+    view: document.getElementById("view"), // Основной фон
+    viewCircle: document.getElementById("view-circle"), // Фон кружочка
+}
 
 var defaultBackgroundColors = {
-    view: getBackgroundColorValue(view),
-    viewCircle: getBackgroundColorValue(viewCircle)
+    view: getBackgroundColorValue(mainElements.view),
+    viewCircle: getBackgroundColorValue(mainElements.viewCircle)
 }
 
 console.log(defaultBackgroundColors)
 
-buttonPower.addEventListener("click", function (){
-    var style = getComputedStyle(buttonPower)
+buttonElements.buttonPower.addEventListener("click", function (){
+    var style = getComputedStyle(buttonElements.buttonPower)
     var red = "rgb(245, 75, 61)"
     var green = "rgb(51, 211, 76)"
     var redShadow = "rgb(245, 75, 61) 0px 0px 8px 2px"
     var  greenShadow = "rgb(51, 211, 76) 0px 0px 8px 2px"
 
-    buttonPower.style.backgroundColor = style.backgroundColor === red ? green : red;
-    buttonPower.style.boxShadow = style.boxShadow === redShadow ? greenShadow : redShadow;
+    buttonElements.buttonPower.style.backgroundColor = style.backgroundColor === red ? green : red;
+    buttonElements.buttonPower.style.boxShadow = style.boxShadow === redShadow ? greenShadow : redShadow;
 
 })
 
-buttonArrowTop.addEventListener("click", function (){
+buttonElements.buttonArrowTop.addEventListener("click", function (){
     console.log("Вы нажали вверх")
-    backgroundStyling(circleBackgroundColors, viewCircle)
+    backgroundStyling(circleBackgroundColors, mainElements.viewCircle)
 })
 
-buttonArrowLeft.addEventListener("click", function (){
+buttonElements.buttonArrowLeft.addEventListener("click", function (){
     console.log("Вы нажали влево")
-    backgroundStyling(bodyBackgroundColors, view)
+    backgroundStyling(bodyBackgroundColors, mainElements.view)
 })
 
-buttonCenter.addEventListener("click", function (){
+buttonElements.buttonCenter.addEventListener("click", function (){
     console.log("Вы нажали на  ОК")
-   //TODO: реализовать изменение фонов на начальное значение
-    // backgroundStyling()
+
+    backgroundStyling([defaultBackgroundColors.view], mainElements.view)
+    backgroundStyling([defaultBackgroundColors.viewCircle], mainElements.viewCircle)
 })
 
-buttonArrowRight.addEventListener("click", function (){
+buttonElements.buttonArrowRight.addEventListener("click", function (){
     console.log("Вы нажали вправо")
-    backgroundStyling(bodyBackgroundColors,view )
+    backgroundStyling(bodyBackgroundColors,mainElements.view )
 })
 
-buttonArrowBotton.addEventListener("click", function (){
+buttonElements.buttonArrowBottom.addEventListener("click", function (){
     console.log("Вы нажали вниз")
-    backgroundStyling(circleBackgroundColors, viewCircle)
+    backgroundStyling(circleBackgroundColors, mainElements.viewCircle)
 })
 
-buttonMainBack.addEventListener("click",function (){
+buttonElements.buttonMainBack.addEventListener("click",function (){
     console.log("Вы вернулись назад")
 })
 
-buttonMainSearch.addEventListener("click", function (){
+buttonElements.buttonMainSearch.addEventListener("click", function (){
     console.log("Вы нажали поиск")
 })
 
-buttonMainSetting.addEventListener("click", function (){
+buttonElements.buttonMainSetting.addEventListener("click", function (){
     console.log("Вы нажали настройки")
 })
 
-buttonMainVoice.addEventListener("click", function (){
+buttonElements.buttonMainVoice.addEventListener("click", function (){
     console.log("Вы нажали микрофон")
 })
 
-buttonMainTurnup.addEventListener("click", function (){
+buttonElements.buttonMainTurnup.addEventListener("click", function (){
     console.log("Вы прибавили звук")
 })
 
-buttonMainTurndown.addEventListener("click", function (){
+buttonElements.buttonMainTurndown.addEventListener("click", function (){
     console.log("Вы убавили звук")
 })
 
@@ -126,13 +107,13 @@ var circleBackgroundColors = [
     "rgba(23, 22, 20, 1)"
 ]
 
-
+// Функция принимает массив цветов и перекрашивает фон у элемента
 function backgroundStyling(arrayOfColors, element) {
     var rand = Math.floor(Math.random() * arrayOfColors.length); //0-5
-    // var style = getComputedStyle(buttonPower)
     element.style.backgroundColor = arrayOfColors[rand]
 }
 
+// Получаем элемент и возвращаем значение backgroundColor у элемента
 function getBackgroundColorValue(element){
     var style = getComputedStyle(element)
     return style.backgroundColor
